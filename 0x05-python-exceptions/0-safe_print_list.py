2081d273b8b99e146abb8d98374860de642be760
llblch <mustqwert@gmail.com>
2022-12-20 20:55:03 -0800

    Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,12 +11,13 @@ Args:
     Returns:
     The number of elements.
 """
-ret = 0
-for i in range(x):
-    try:
-        print("{}".format(my_list[i]), end="")
-        ret += 1
-    except IndexError:
-        break
-print("")
-return (ret)
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total += 1
+        except IndexError:
+            break
+    print()
+    return(total)
